126ee2a424bf2673aca73c84913735cc4e9c2c5c
Daniil Kuliev <danilkakuliew@gmail.com>
2022-03-10 17:31:38 +0300

    added anim

---
+++ WeatherApp/Models/KelvinToCeilsConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    internal class KelvinToCeilsConverter : ITemperatureConverter
+    {
+        public float Convert(float value) => value - 273;
+    }
+}
--- WeatherApp/ViewModels/MainWindowViewModel.cs
+++ WeatherApp/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Threading;
+using WeatherApp.Models;
 using WeatherApp.Models.Responses;
 using WeatherApp.Properties;
 using WeatherApp.Services;
@@ -25,6 +27,16 @@ namespace WeatherApp.ViewModels
             }
         }
 
+        public bool WeatherLoading
+        {
+            get => _weatherLoading;
+            set
+            {
+                _weatherLoading = value;
+                OnPropertyChanged(nameof(WeatherLoading));
+            }
+        }
+
         public WeatherResponse CurrentWeather
         {
             get => _weatherResponse;
@@ -35,19 +47,42 @@ namespace WeatherApp.ViewModels
             }
         }
 
+        public string CurrentWeatherImage
+        {
+            get => _currentWeatherImage;
+            set
+            {
+                _currentWeatherImage = value;
+                OnPropertyChanged(nameof(CurrentWeatherImage));
+            }
+        }
+
         public MainWindowViewModel()
         {
             _weatherService = new WeatherService(new HttpService(), Resources.apiKey);
+            WeatherLoading = false;
         }
 
         private async Task LoadWeather()
         {
+            WeatherLoading = true;
             Response response = await _weatherService.GetWeatherAsync(City);
             switch (response)
             {
                 case ErrorResponse errorResponse:
                     break;
-                case WeatherResponse weatherResponse: CurrentWeather = weatherResponse; break;
+                case WeatherResponse weatherResponse:
+                    await Task.Delay(1000); // Это для того, чтобы анимация не дергалась, если данные приходят сразу
+                    KelvinToCeilsConverter kelvinToCeils = new KelvinToCeilsConverter();
+                    weatherResponse.Main.TempMax = (float)Math.Round(kelvinToCeils.Convert(weatherResponse.Main.TempMax), 1);
+                    weatherResponse.Main.TempMin = (float)Math.Round(kelvinToCeils.Convert(weatherResponse.Main.TempMin), 1);
+                    weatherResponse.Main.FeelsLike = (float)Math.Round(kelvinToCeils.Convert(weatherResponse.Main.FeelsLike), 1);
+                    weatherResponse.Main.Temp = (float)Math.Round(kelvinToCeils.Convert(weatherResponse.Main.Temp), 1);
+
+                    CurrentWeather = weatherResponse;
+                    CurrentWeatherImage = $"/Assets/{CurrentWeather.Weather[0].Main.ToLower()}.png";
+                    WeatherLoading = false;
+                    break;
             }
         }
 
@@ -56,5 +91,9 @@ namespace WeatherApp.ViewModels
         private WeatherResponse _weatherResponse;
 
         private string _city;
+
+        private string _currentWeatherImage;
+
+        private bool _weatherLoading;
     }
 }
